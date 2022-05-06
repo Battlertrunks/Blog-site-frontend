eaa935dc38163933967958b91471c208787c960a
Gavin <Gavinszczesniak@gmail.com>
2022-05-06 12:45:26 -0400

    Filled out the about page, will add more in the future.

--- src/components/AboutComponent/AboutRoute.tsx
+++ src/components/AboutComponent/AboutRoute.tsx
@@ -1,7 +1,42 @@
 import "./AboutRoute.css";
 
 const AboutRoute = () => {
-  return <div className="AboutRoute">AboutRoute works</div>;
+  return (
+    <section className="AboutRoute">
+      <h2>About us</h2>
+      <h3>Who we are</h3>
+      <p>
+        Our company is to share the wonders about programming, skills, tools,
+        and as a whole, knowledge about the industry.
+      </p>
+      <p>
+        We share our skills and process to new or curious developers to be
+        update to date on the lastest software and those who seek to learn more.
+      </p>
+      <h3>Our future goals</h3>
+      <p>
+        We hope to build out this blog site to be more informative and feature
+        better user experience. This website for the moment is not close to
+        being done, but it is a start. Updates will be released to improve this
+        site's design, performace, and include new features.
+      </p>
+      <h3>The team</h3>
+      <h4>Gavin Szczesniak</h4>
+      <p>
+        Gavin is a determined developer that strives to improve himself to
+        become a better developer. He looks for the weaknesses he has in order
+        to turn them into his strengthes in his developer skill set.
+      </p>
+      <p>
+        He is a MERN stack developer that is currently learning ASP .NET and
+        Angular to be a more valueable developer.
+      </p>
+      <p>
+        During his free time, he like to spend time with his significant other
+        and taking care of his two cats and dog.
+      </p>
+    </section>
+  );
 };
 
 export default AboutRoute;
--- src/components/BlogFormComponent/BlogFormRoute.tsx
+++ src/components/BlogFormComponent/BlogFormRoute.tsx
@@ -1,7 +1,22 @@
 import "./BlogFormRoute.css";
 
 const BlogFormRoute = () => {
-  return <div className="BlogFormRoute">BlogFormRoute works</div>;
+  return (
+    <form className="BlogFormRoute">
+      <h3>Creating a Post</h3>
+      <label htmlFor="title">Title</label>
+      <input type="text" name="title" id="title" />
+      <label htmlFor="opening-img">Opening Picture</label>
+      <input type="file" name="opening-img" id="opening-img" />
+      <label htmlFor="content">Content</label>
+      <textarea
+        name="content"
+        id="content"
+        placeholder="Type content here..."
+      ></textarea>
+      <button>Submit</button>
+    </form>
+  );
 };
 
 export default BlogFormRoute;
