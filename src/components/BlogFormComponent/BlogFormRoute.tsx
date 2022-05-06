b939297f852fb2f9e34323f6922268a494334753
Gavin <Gavinszczesniak@gmail.com>
2022-05-06 13:05:58 -0400

    Added CSS to the contact page with contact information. Worked on the post form, giving it state and a submit handler.

--- src/components/BlogFormComponent/BlogFormRoute.tsx
+++ src/components/BlogFormComponent/BlogFormRoute.tsx
@@ -1,18 +1,47 @@
+import { FormEvent, useState } from "react";
 import "./BlogFormRoute.css";
 
 const BlogFormRoute = () => {
+  const [title, setTitle] = useState<string>("");
+  const [picture, setPicture] = useState<string>("");
+  const [body, setBody] = useState<string>("");
+
+  const submitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    setTitle("");
+    setPicture("");
+    setBody("");
+  };
+
   return (
-    <form className="BlogFormRoute">
+    <form className="BlogFormRoute" onSubmit={submitHandler}>
       <h3>Creating a Post</h3>
       <label htmlFor="title">Title</label>
-      <input type="text" name="title" id="title" />
+      <input
+        type="text"
+        name="title"
+        id="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
       <label htmlFor="opening-img">Opening Picture</label>
-      <input type="file" name="opening-img" id="opening-img" />
+      <input
+        type="file"
+        name="opening-img"
+        id="opening-img"
+        value={picture}
+        onChange={(e) => setPicture(e.target.value)}
+        required
+      />
       <label htmlFor="content">Content</label>
       <textarea
         name="content"
         id="content"
         placeholder="Type content here..."
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        required
       ></textarea>
       <button>Submit</button>
     </form>
--- src/components/ContactComponent/ContactRoute.tsx
+++ src/components/ContactComponent/ContactRoute.tsx
@@ -1,7 +1,20 @@
 import "./ContactRoute.css";
 
 const ContactRoute = () => {
-  return <div className="ContactRoute">ContactRoute works</div>;
+  return (
+    <section className="ContactRoute">
+      <h2>Contact Us</h2>
+      <h3>Email:</h3>
+      <p>For general support and questions, please email this address: </p>
+      <p>support@typeascript.com</p>
+      <h3>Phone</h3>
+      <p>For quick general support and questions, plase call this number:</p>
+      <p>123-456-7890</p>
+      <h3>Our headquarters address:</h3>
+      <p>12345 uphill dr</p>
+      <p>Portland, OR 12345</p>
+    </section>
+  );
 };
 
 export default ContactRoute;
